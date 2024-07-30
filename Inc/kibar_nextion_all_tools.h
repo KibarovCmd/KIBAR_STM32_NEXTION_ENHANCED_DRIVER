11f7315a0a99e8ba79d6836751093d98b84b5ef8
Alexander Kibarov <117196657+KibarovCmd@users.noreply.github.com>
2024-07-31 00:45:13 +0300

    Add files via upload
    Added kibar_nextion_all_tools.h to be able to add all header files with a single header file.

---
+++ Inc/kibar_nextion_all_tools.h
@@ -0,0 +1,30 @@
+/*
+ * kibar_nextion_all_tools.h
+ *
+ *  Created on: Jul 31, 2024
+ *      Author: isken
+ */
+
+#ifndef KIBAR_NEXTION_INC_KIBAR_NEXTION_ALL_TOOLS_H_
+#define KIBAR_NEXTION_INC_KIBAR_NEXTION_ALL_TOOLS_H_
+
+#include <kibar_nextion_button.h>
+#include <kibar_nextion_check_box.h>
+#include <kibar_nextion_crop.h>
+#include <kibar_nextion_dual_state_button.h>
+#include <kibar_nextion_gauge.h>
+#include <kibar_nextion_number.h>
+#include <kibar_nextion_page.h>
+#include <kibar_nextion_picture.h>
+#include <kibar_nextion_progress_bar.h>
+#include <kibar_nextion_qrcode.h>
+#include <kibar_nextion_radio.h>
+#include <kibar_nextion_scrolling_text.h>
+#include <kibar_nextion_slider.h>
+#include <kibar_nextion_text.h>
+#include <kibar_nextion_timer.h>
+#include <kibar_nextion_variable.h>
+#include <kibar_nextion_waveform.h>
+#include <kibar_nextion_xfloat.h>
+
+#endif /* KIBAR_NEXTION_INC_KIBAR_NEXTION_ALL_TOOLS_H_ */
